de94f2e15c87b75ad26aa3e339725fddc353f22c
abir <ahammed.abir47@gmail.com>
2021-02-24 14:24:24 -0600

    Formed class using all the existing components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import Search from './search/Search';
+import { Component } from 'react';
 
-function App() {
 
-  return (
-    <div className="App">
-      <header className="App-header">
-      <Search/>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <Search/>
+        </header>
+      </div>);
+  }
 }
 
 export default App;
-
--- src/search/Search.js
+++ src/search/Search.js
@@ -1,23 +1,34 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 
 import SearchBar from "material-ui-search-bar";
 
 import './Search.css';
+import { render } from '@testing-library/react';
 
-function doSomethingWith(state) {
-    console.log(state);
-}
-function Search() {
-    const [state, setState] = useState(0);
 
-    return (
-        <SearchBar
-    value={"Search me"}
-    onChange={(newValue) => setState(newValue)}
-    // onChange={(newValue) => this.setState({ value: newValue })}
-    onRequestSearch={() => doSomethingWith(state)}
-  />
-    );
+class Search extends Component {
+    constructor(props) {
+        super(props);
+      }
+
+    // doSomethingWith(state) {
+    //     console.log(state);
+    // }
+    doSomethingWith = (state) => {
+        console.log(state);
+      };
+
+    render() {
+        return (
+            <SearchBar
+        value={this.props.placeholder}
+        onChange={(newValue) => this.props.placeholder = newValue}
+        // onChange={(newValue) => this.setState({ value: newValue })}
+        onRequestSearch={() => this.doSomethingWith(this.props.placeholder)}
+      />
+        );
+    }
+    
 }
 
 export default Search;
